b460b5f5c89589c8bf9b79c2060221c37a8591fb
Ikenna Richard <oguejioforrichard@gmail.com>
2024-03-10 01:53:32 +0100

    feat: ✨ Add route for getting timestamp

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,27 @@
 import 'dotenv/config';
 import express from 'express';
+import isValid from './isValid';
 
 const app = express();
+
+app.use(express.json());
+
+app.get('/api/:dateEntry', (req, res) => {
+  const { dateEntry } = req.params;
+  const date = new Date(dateEntry);
+
+  // check if date is valid
+  if (isValid(date.getTime())) {
+    return res.json({
+      "unix": date.getTime(),
+      "utc": date.toUTCString()})
+  } else {
+    return res.status(400).json({"error": "Invalid date"})
+  }
+});
 
 app.listen(process.env.PORT, () => (
   console.log(
     `🚀 Server is running at localhost:${process.env.PORT}`
   )
-))
+));
