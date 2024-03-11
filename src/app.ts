e1e7cdf2f90f6d8dce35f856d59afb74c7e993ca
Ikenna Richard <oguejioforrichard@gmail.com>
2024-03-11 16:36:27 +0100

    feat: ✨ Add regex for testing if entry contains only numbers

--- src/app.ts
+++ src/app.ts
@@ -3,25 +3,34 @@ import express from 'express';
 import isValid from './isValid';
 
 const app = express();
+const port = process.env.PORT;
 
 app.use(express.json());
 
 app.get('/api/:dateEntry', (req, res) => {
   const { dateEntry } = req.params;
-  const date = new Date(dateEntry);
+  let date: Date;
 
-  // check if date is valid
-  if (isValid(date.getTime())) {
-    return res.json({
-      "unix": date.getTime(),
-      "utc": date.toUTCString()})
+  // check if entry contains only numbers 
+  if (/^\d+$/.test(dateEntry)) {
+    date = new Date(Number(dateEntry));
   } else {
-    return res.status(400).json({"error": "Invalid date"})
+    date = new Date(dateEntry);
   }
+  
+  // check if date is valid
+  if (!isValid(date.getTime())) {
+    return res.status(400).json({"error": "Invalid date"});
+  }
+
+  return res.json({
+    "unix": date.getTime(),
+    "utc": date.toUTCString()
+  });
 });
 
-app.listen(process.env.PORT, () => (
+app.listen(port, () => (
   console.log(
-    `🚀 Server is running at localhost:${process.env.PORT}`
+    `🚀 Server is running at localhost:${port}`
   )
 ));
