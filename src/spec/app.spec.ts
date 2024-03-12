90dadd5acb47fb80a07822e9dd28646290bc40cd
Ikenna Richard <oguejioforrichard@gmail.com>
2024-03-12 02:05:20 +0100

    test: ✅ Add route test

---
+++ src/spec/app.spec.ts
@@ -0,0 +1,21 @@
+import { response } from "express";
+import { app } from "../app";
+import request from "supertest";
+
+describe("Test the path", () => {
+  test("It should response the GET method", () => {
+    return request(app)
+      .get("/api/2024-02-19")
+      .expect(200);
+  });
+
+  test("It should response the GET method", async () => {
+    const response = await request(app)
+      .get("/api/2024-02-19")
+    expect(JSON.parse(response.text))
+    .toEqual({
+      "unix":1708300800000,"utc":
+      "Mon, 19 Feb 2024 00:00:00 GMT"
+    })
+  });
+});
